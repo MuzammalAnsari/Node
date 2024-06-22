88392c81d6a599063500b6bb08872251d346550f
Muzammal Ansari <muzammalbash@gmail.com>
2024-06-22 03:59:07 -0700

    added lodash library

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const { log } = require('console');
-var fs = require('fs')
-var os = require('os')
-
-var user = os.userInfo()
-console.log(user);
-
-fs.appendFile('greeting.txt', 'Hi '+ user.username + '!',()=>console.log('file created') )
-
-// console.log(os);
+var _ = require('lodash')
