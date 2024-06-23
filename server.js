2d8c5bc52f6d3104ba64791c237d541991aa7ffb
Muzammal Ansari <muzammalbash@gmail.com>
2024-06-23 03:06:52 -0700

    added get function and created server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-var _ = require('lodash')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/chicken', (req, res) => {
+    var chicken={
+        age:1,
+        quantity:2,
+        name:'hen'
+    } 
+    res.send(chicken)
+})
+
+
+
+app.listen(3000, ()=>{
+    console.log('server is running on port 3000')
+})
