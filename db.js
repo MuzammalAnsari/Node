5d24ebc8761cd0d354271f5be6e9b220dec668c1
Muzammal Ansari <muzammalbash@gmail.com>
2024-06-25 05:09:59 -0700

    connected with mongodb and also data is being and other actions also perfomed on data

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 // Connect to the database
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels'
+// const mongoURL = process.env.MONGODB_URL_LOCAL
+
+   const mongoURL = process.env.MONGODB_URL
+
 
 // Connect to the database
 mongoose.connect(mongoURL, {
@@ -14,7 +18,9 @@ const db = mongoose.connection;
 
 //defines event listener for db connection
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', () => {
+    console.log('Error occurred while connecting to the database');
+});
 
 db.on('connected', ()=>{
     console.log('connected to DB server')
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+require('dotenv').config()
 
 
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());     //req.body
+
+const PORT = process.env.PORT || 3000
 
 
 app.get('/', (req, res) => {
@@ -37,6 +40,7 @@ app.use('/person', personRoutes)
 //import the menuItem route file
 const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/menu', menuItemRoutes)
+
 
 
 
